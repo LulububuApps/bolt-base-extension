4cca7590ac92f0059e2e4505dfb5f92c27f3e162
Joshua Schumacher <joshua@lulububu.de>
2021-04-18 06:37:49 +0200

    Fixed the required select field

--- assets/lulububu/lulububu.js
+++ assets/lulububu/lulububu.js
@@ -1,3 +1,22 @@
-$(document).ready(() => {
-    console.log('READY');
-});
+const initializeSelectFields = () => {
+    const multiSelectWrappers = document.querySelectorAll('.multiselect-bolt-wrapper');
+
+    multiSelectWrappers.forEach((multiSelectWrapper) => {
+        const isRequired = multiSelectWrapper.previousElementSibling.querySelector('span.required-label');
+
+        if (isRequired) {
+            const inputField    = multiSelectWrapper.querySelector('input[type="hidden"]');
+            inputField.type     = 'text';
+            inputField.value    = inputField.value !== '[]' ? inputField.value : null;
+            inputField.required = true;
+
+            inputField.classList.add('require-select');
+        }
+    });
+};
+
+const initialize = () => {
+    initializeSelectFields();
+};
+
+document.addEventListener('DOMContentLoaded', initialize, false);
